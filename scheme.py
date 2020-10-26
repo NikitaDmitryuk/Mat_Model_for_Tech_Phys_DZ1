e3ecec8e4e50bd9c98a93d406d3869487493bc79
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2020-10-26 22:34:27 +0300

    create jupyter notebook

--- scheme.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Scheme(ABC):
-
-    @abstractmethod
-    def solve(self, grid, initial_conditions, border_conditions):
-        """Решение дифференциального уравнения"""
